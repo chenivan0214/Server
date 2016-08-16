178190db98e9dbadce49e18e680bc381f1236d20
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-08-16 17:32:40 +0800

    Change: use mod prefix

--- Nodejs/Express/src/app.js
+++ Nodejs/Express/src/app.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var objExpress = require('express');
-var objApp = objExpress();
-var objInitApp = require('./library/initApp.libary.js');
-var objFs = require("fs");
+var modExpress = require('express');
+var modApp = modExpress();
+var modInitApp = require('./library/initApp.libary.js');
+var modFs = require("fs");
 
 //init app
-objInitApp.init(objApp, objExpress);
+modInitApp.init(modApp, modExpress);
 
 //load general route
-objFs.readdirSync(objInitApp.data.path.route.general).forEach(function(file) {
-    require(objInitApp.data.path.route.general + file).url(objApp);
+modFs.readdirSync(modInitApp.data.path.route.general).forEach(function(file) {
+    require(modInitApp.data.path.route.general + file).url(modApp);
 });
 
 //load other route
-objFs.readdirSync(objInitApp.data.path.route.other).forEach(function(file) {
-    require(objInitApp.data.path.route.other + file).url(objApp);
+modFs.readdirSync(modInitApp.data.path.route.other).forEach(function(file) {
+    require(modInitApp.data.path.route.other + file).url(modApp);
 });
 
 //listen app
-objInitApp.listen(objApp);
+modInitApp.listen(modApp);

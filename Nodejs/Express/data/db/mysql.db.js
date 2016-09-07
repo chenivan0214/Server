3e418184d046941dd2429905aa918da786556630
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-09-07 16:06:09 +0800

    Change: add query of msyql function

---
+++ Nodejs/Express/data/db/mysql.db.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var modMysql = require('mysql');
+
+exports.query = function(app, sql, callback) {
+    var objDefine = require(app.data.path.db.define + "mysql.define.js"),
+        objDb = modMysql.createConnection(objDefine);
+
+    objDb.connect();
+    objDb.query(sql, function(err, result, field) {
+        err && console.log(err);
+        callback(result);
+    });
+
+    objDb.end();
+}

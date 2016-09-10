d8339b8361f82f0c75a1e68075bded670556bdd6
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-09-10 09:51:30 +0800

    Change: change mysql.library.js what work is

--- Nodejs/Express/src/library/db/mysql.library.js
+++ Nodejs/Express/src/library/db/mysql.library.js
@@ -2,15 +2,9 @@
 
 var modMysql = require('mysql');
 
-exports.select = function(app, sql, callback) {
+exports.init = function(app) {
     var objDefine = require(app.data.path.db.root + "mysql.js"),
         objDb = modMysql.createConnection(objDefine);
 
-    objDb.connect();
-    objDb.query(sql, function(err, result, field) {
-        if (err) console.log(err);
-        callback(result);
-    });
-
-    objDb.end();
+    return objDb;
 };
